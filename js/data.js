713a0b2635be24b6060fb2098ff0a1907afe1d2a
Josh <gertner.code@gmail.com>
2019-03-11 22:30:36 -0400

    first commit

---
+++ js/data.js
@@ -0,0 +1,38 @@
+//Model Data
+var games = [
+  /* Example
+  {
+      title: ,
+      status: ,
+      genre: ,
+      company: ,
+      role: ,
+      engine: ,
+      team_size: ,
+      description: ,
+      work_done: ,
+      main_img: ,
+      screeenshots: [],
+      weblink: ,
+      android: ,
+      apple: ,
+      visibility: false
+    },*/
+  {
+    title: "",
+    status: "",
+    genre: "",
+    company: "",
+    role: "",
+    engine: "",
+    team_size: "",
+    description: "",
+    work_done: "",
+    main_img: "",
+    screeenshots: [""],
+    weblink: "",
+    android: "",
+    apple: "",
+    visibility: false //always false please
+  }
+];
---
+++ js/main.js
@@ -0,0 +1,12 @@
+function ViewModel() {
+  var self = this;
+  self.games = ko.observableArray(games);
+  self.visiblenow = function(game) {
+    self.games.visibility = true;
+  };
+  self.frontpage = games.forEach(function(game) {
+    self.visibility = false;
+  });
+}
+
+ko.applyBindings(new ViewModel());
