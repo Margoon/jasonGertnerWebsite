b5ebae7d4f98731ebb70d6de998f6cf5a7dfb972
Josh <gertner.code@gmail.com>
2019-03-13 11:03:55 -0400

    Content: Added new images used in data

--- js/data.js
+++ js/data.js
@@ -2,37 +2,75 @@
 var games = [
   /* Example
   {
-      title: ,
-      status: ,
-      genre: ,
-      company: ,
-      role: ,
-      engine: ,
-      team_size: ,
-      description: ,
-      work_done: ,
-      main_img: ,
-      screeenshots: [],
-      weblink: ,
-      android: ,
-      apple: ,
-      visibility: false
+      title: "", string
+      status: "", string
+      genre: "", string
+      company: "", string
+      role: "", string
+      engine: "", string
+      team_size: "", string
+      description: "", string
+      work_done: "", string
+      main_img: "img/", string write img/ before all image names, images go in img folder
+      screeenshots: ["img/"], strings
+      links: , boolean for whether you have links
+      weblink: "", string
+      android: "", string
+      apple: "", string
+      visibility: false leave false always
     },*/
   {
-    title: "",
-    status: "",
-    genre: "",
-    company: "",
-    role: "",
-    engine: "",
-    team_size: "",
+    title: "Burning Wood",
+    status: "Released February 2017",
+    genre: "Arcade",
+    company: "Shattered Journal Games",
+    role: "Programmer/Designer",
+    engine: "GameMaker: Studio",
+    team_size: "2",
+    description: " Can YOU burn all the wood? How far can you get? Are you SKILLED enough to dodge the raindrops? See how long you can last, and beat your friends’s scores!!",
+    work_done: "Burning Wood was a small game I made in a game jam in 2 hours in February 2017 in my senior year of high school. I thought the idea was good enough to put it on phones, so I revamped it over the course of a couple weeks. This was my first game released anywhere, and I learned a lot through just the experience of making it and releasing it. Tons of people at my school were playing it, so many that some people didn't even know I made it! I always like to use Burning Wood as an example of how simple games can be fun and easy for anyone to jump in and play.",
+    main_img: "img/burningWoodIcon512.png",
+    screeenshots: ["img/screenIOS1.png", "img/screenIOS2.png", "img/screenIOS3.png.png", "img/screenIOS3.png.png"],
+    links: true,
+    weblink: "",
+    android: "https://play.google.com/store/apps/details?id=com.Shattered_Journal_Games.BURNING_WOOD",
+    apple: "https://itunes.apple.com/gb/app/burning-wood/id1209788363",
+    visibility: false //always false please
+  },
+  {
+    title: "Cloud Bashers",
+    status: "Released September 2017",
+    genre: "Arcade",
+    company: "Shattered Journal Games",
+    role: "Programmer/Designer",
+    engine: "GameMaker: Studio",
+    team_size: "2",
+    description: "Fly on a cloud with your trusty bat and take back the skies! Bash enough clouds to take out all the bosses!",
+    work_done: "Cloud Bashers was a game I released at the beginning of my first year of college. A lot of people knew about the game because I made fliers and spread them everywhere on campus. Since release the game has gained a small cult following because of the games personality and marketing.",
+    main_img: "img/cloudBasherIcon512.png",
+    screeenshots: ["img/screenshot#1.png", "img/screenshot#2.png", "img/screenshot#3.png", "img/screenshot#4.png", "img/screenshot#5.png"],
+    links: true,
+    weblink: "",
+    android: "https://play.google.com/store/apps/details?id=com.Shattered_Journal_Games.Cloud_Bashers",
+    apple: "https://itunes.apple.com/us/app/cloud-bashers/id1227986698",
+    visibility: false
+  },
+  {
+    title: "Leap A Head",
+    status: "Released April 5th 2018",
+    genre: "Puzzle",
+    company: "MassDiGI",
+    role: "Programmer/QA",
+    engine: "Unity",
+    team_size: "6+",
     description: "",
     work_done: "",
-    main_img: "",
+    main_img: "img/",
     screeenshots: [""],
+    links: true,
     weblink: "",
     android: "",
     apple: "",
-    visibility: false //always false please
+    visibility: false
   }
 ];
